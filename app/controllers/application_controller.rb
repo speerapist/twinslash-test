a291314b980127c80fac152943db3259a83bf90f
speerapist <v.lukyantsou@icloud.com>
2020-10-01 17:55:57 +0300

    Fixed regestration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from CanCan::AccessDenied do
     redirect_to main_app.root_path, notice: 'You have not permission to do this action'
   end
@@ -10,5 +11,10 @@ class ApplicationController < ActionController::Base
 
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
